660c10f5ec63a641f2bff4c38886cd2784d54cae
Brenton Collins <116970509+bjc2012@users.noreply.github.com>
2023-05-17 00:30:24 -0500

    Create python-calculator.py

---
+++ python-calculator.py
@@ -0,0 +1,18 @@
+# Advanced Calculator Build that will Add, Subtract, Multpily, Divide
+# The user will be able to specify which operation they'd like to use
+
+num1 = float(input("Enter first number: "))
+op = input("Enter operator: ")
+num2 = float(input("Enter second number: "))
+
+if op == "+":
+    print(num1 + num2)
+elif op == "-":
+    print(num1 - num2)
+elif op == "/":
+    print(num1 / num2)
+elif op == "*":
+    print(num1 * num2)
+else:
+    print("incorrect operator")
+    
